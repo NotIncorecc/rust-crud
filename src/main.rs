a8848a9395cc63863a37911487ebf0f9e62bdda1
Gokul G <gane.incore@gmail.com>
2025-02-15 23:25:36 +0530

    simple web browser

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use actix_web::{App, HttpServer, Responder};
+
+#[actix_web::get("/greet")]
+async fn greet() -> impl Responder {
+    format!("Hello World")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let port: u16= 8080;
+    println!("Starting web server on port {port}");
+
+    HttpServer::new(|| App::new().service(greet))
+        .bind(("127.0.0.1", port))?
+        .workers(2)
+        .run()
+        .await
+}
